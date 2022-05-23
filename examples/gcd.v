8df4cb1abf45a56415ef1c7417669167c6a8f68b
Alex Gryzlov <aliaksandr.hryzlou@imdea.org>
2022-05-23 14:17:59 +0200

    refactor

--- examples/gcd.v
+++ examples/gcd.v
@@ -7,51 +7,49 @@ Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 Obligation Tactic := auto.
 
-Definition pair (p q : ptr) (a b : nat) :=
-  [Pred h | exists h', h = p :-> a \+ (q :-> b \+ h')].
+Definition pair (p q : ptr) (x y : nat) :=
+  [Pred h | exists h', h = p :-> x \+ (q :-> y \+ h')].
 
-Definition gcd_loopT (x y : ptr) : Type :=
+Definition gcd_loopT (p q : ptr) : Type :=
   unit ->
-  {a b : nat}, STsep (pair x y a b,
+  {x y : nat}, STsep (pair p q x y,
                      [vfun (_ : unit) h =>
-                        h \In pair x y (gcdn a b) (gcdn a b)]).
+                        h \In pair p q (gcdn x y) (gcdn x y)]).
 
-Program Definition gcd_loop (a b : ptr) : gcd_loopT a b :=
-  Fix (fun (go : gcd_loopT a b) _ =>
-       Do (x <-- !a;
-           y <-- !b;
+Program Definition gcd_loop (p q : ptr) : gcd_loopT p q :=
+  Fix (fun (go : gcd_loopT p q) _ =>
+       Do (x <-- !p;
+           y <-- !q;
            if (x : nat) != y then
-             if x < y then b ::= y - x;;
+             if x < y then q ::= y - x;;
                            go tt
-                      else a ::= x - y;;
+                      else p ::= x - y;;
                            go tt
            else ret tt)).
 Next Obligation.
-move=>a b  go _ [x [y _]] /= _ [h' ->].
-do 2!step; case: eqP=>/=.
-- move=>->; step=>_.
-  by rewrite gcdnn; vauto.
-move=>E; case: ltngtP=>// {E}H; step.
-- apply: [gE x, (y-x)]=>/=; first by eauto.
+move=>p q go _ [x [y _]] /= _ [h' ->].
+do 2!step; case: ltngtP=>/= [H|H|->]; step; last first.
+- by move=>_; rewrite gcdnn; vauto.
+- apply: [gE (x-y), y]=>//=; first by eauto.
   case=>//= _ _ _ [m ->]; exists m.
-  suff: gcdn x (y - x) = gcdn x y by move=>->.
-  by rewrite -gcdnDr subnK //; apply: ltnW.
-apply: [gE (x-y), y]=>//=; first by eauto.
+  suff: gcdn (x - y) y = gcdn x y by move=>->.
+  by rewrite gcdnC -gcdnDr gcdnC subnK //; apply: ltnW.
+apply: [gE x, (y-x)]=>/=; first by eauto.
 case=>//= _ _ _ [m ->]; exists m.
-suff: gcdn (x - y) y = gcdn x y by move=>->.
-by rewrite gcdnC -gcdnDr gcdnC subnK //; apply: ltnW.
+suff: gcdn x (y - x) = gcdn x y by move=>->.
+by rewrite -gcdnDr subnK //; apply: ltnW.
 Qed.
 
 (* note that there's no guarantee on termination *)
 (* i.e. the algorithm will get stuck if u = 0 /\ v != 0 or vice versa *)
 Program Definition gcd u v :
-  STsep (PredT, [vfun r h => r = gcdn u v]) :=
-  Do (a <-- alloc u;
-      b <-- alloc v;
-      gcd_loop a b tt;;
-      x <-- !a;
-      dealloc a;;
-      dealloc b;;
+  STsep (PredT, [vfun r _ => r = gcdn u v]) :=
+  Do (p <-- alloc u;
+      q <-- alloc v;
+      gcd_loop p q tt;;
+      x <-- !p;
+      dealloc p;;
+      dealloc q;;
       ret x).
 Next Obligation.
 move=>u v _ m /= _.
