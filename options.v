0139204b650d7eda33fc22bb2b61b77a65b616be
Alekandar Nanevski <aleks.nanevski@imdea.org>
2022-11-08 16:14:43 +0100

    blah

---
+++ options.v
@@ -0,0 +1,7 @@
+Export Set Implicit Arguments.
+Export Unset Strict Implicit.
+Export Unset Printing Implicit Defensive.
+Export Unset Program Cases.
+
+(* turn off the automation of Program *)
+Obligation Tactic := auto.
